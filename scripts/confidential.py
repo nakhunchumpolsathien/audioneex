2109561ff9ad45159f1640683697d885808ebf02
Nakhun Chumpolsathien <45205446+nakhunchumpolsathien@users.noreply.github.com>
2022-03-28 12:06:05 +0700

    add example of confidential.py

---
+++ scripts/confidential.py
@@ -0,0 +1,17 @@
+#Example
+class Confidential:
+    output_csv = 'test.csv'
+    classify_log_path = ''
+
+    iwads_host = ''
+    iwads_username = ''
+    iwads_password = ''
+    iwads_db_name = ''
+    iwads_port = 0000
+    query_brand_command = ''
+
+    classifier = ''
+    xscale_db = ''
+    xscale_fingerprinting_log = ''
+    mscale_db = ''
+    audios_24_hour_dir = ''
